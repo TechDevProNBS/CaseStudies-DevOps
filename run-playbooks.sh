4890e557d939732e2edac8cdc67676018964c67a
SamBritten <sbritten315@gmail.com>
2019-12-12 09:59:13 +0000

    updated run-playbooks script

--- run-playbooks.sh
+++ run-playbooks.sh
@@ -14,6 +14,6 @@ ansible-playbook \
 
 #DOCKER
 ansible-playbook \
-    -i "$(terraform output jenkins_fqdn)," \
+    -i "$(terraform output nginx_fqdn), $(terraform output jenkins_fqdn), $(terraform output mysql_fqdn), $(terraform output eureka_fqdn), $(terraform output node_fqdn), $(terraform output springboot_fqdn), $(terraform output zuul_fqdn)," \
     -u sam \
     --ssh-common-args="-o StrictHostKeyChecking=no" playbooks/docker.yml
